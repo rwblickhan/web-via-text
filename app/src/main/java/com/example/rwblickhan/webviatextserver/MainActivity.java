fe5f014682dc9565d6e5857884bdf586ce5d9ea4
rwblickhan <rwblickhan@gmail.com>
2017-05-17 18:28:22 -0700

    Add sms broadcast receiver

--- app/src/main/java/com/example/rwblickhan/webviatextserver/MainActivity.java
+++ app/src/main/java/com/example/rwblickhan/webviatextserver/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.rwblickhan.webviatextserver;
 
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity inst;
+    private static final int URL_PARSED = 1;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onUrlParse(Object url) {
+        Message msg = Message.obtain();
+
+        //TODO add handler
+        msg.setTarget(/*handler here*/);
+        msg.what = URL_PARSED;
+        msg.obj = url;
+
+        msg.sendToTarget();
+    }
+
 }
---
+++ app/src/main/java/com/example/rwblickhan/webviatextserver/SmsBroadcastReceiver.java
@@ -0,0 +1,44 @@
+package com.example.rwblickhan.webviatextserver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+
+import java.util.HashMap;
+
+/**
+ * Created by rwblickhan on 5/17/17.
+ */
+
+public class SmsBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String SMS_BUNDLE = "pdus";
+
+    public void onReceive(Context context, Intent intent) {
+        Bundle intentExtras = intent.getExtras();
+
+        String url = "";
+        String address = "";
+        if (intentExtras != null) {
+            Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
+            if (sms != null) {
+                for (Object item : sms) {
+                    String format = intentExtras.getString("format");
+                    SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) item, format);
+
+                    url = smsMessage.getMessageBody();
+                    address = smsMessage.getOriginatingAddress();
+                }
+            }
+            //TODO error handling
+        }
+
+        //TODO check that sms body is a valid url
+        MainActivity inst = MainActivity.instance();
+        HashMap<String, String> obj = new HashMap<>();
+        obj.put(address, url);
+        inst.onUrlParse(obj);
+    }
+}
