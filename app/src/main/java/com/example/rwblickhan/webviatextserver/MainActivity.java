d5f4865876262bda72ead161d163c3efddbe4f35
jiahuahl <jiahuahlee@hotmail.com>
2017-05-17 18:18:03 -0700

    Add event loop

--- app/src/main/java/com/example/rwblickhan/webviatextserver/MainActivity.java
+++ app/src/main/java/com/example/rwblickhan/webviatextserver/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.rwblickhan.webviatextserver;
 
+import android.os.Handler;
+import android.os.Message;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RECEIVE_SMS = 1;
+    Handler mHandler = new SMSHandler();
+
+    static class SMSHandler extends Handler
+    {
+        public void handleMessage(Message msg)
+        {
+            sendSMS(parseAndFetchContent(msg));
+        }
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Looper.prepare();
+        Looper.loop();
+    }
+
+    public void onResult(Object result)
+    {
+        Message msg = Message.obtain();
+
+        msg.setTarget(mHandler);
+        msg.what = RECEIVE_SMS;
+        msg.obj = result;
+
+        msg.sendToTarget();
+    }
+
+    static public String parseAndFetchContent(Message msg)
+    {
+        return "";
+    }
+
+    static public void sendSMS(String str)
+    {
+
     }
 }
